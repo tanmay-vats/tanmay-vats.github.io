8e7813fb28c1e857528185516832d418ab8bb7ff
Tanmay Vats <tanmay.vats@engineer.ai>
2022-02-01 20:41:02 +0530

    deletd extra files

--- template-part/search.php
+++
@@ -1,6 +0,0 @@
-<div class="header-search">
-	<form role="search" method="get" id="search-form" action="#">
-		<input type="text" value="" name="s" id="search" placeholder="ENTER KEYWORD">
-		<input type="submit" id="search-submit" title="Search" value="?">
-	</form>
-</div>
